b5901304a8de4f0f0f2e12dc86a257a2945b8799
abe <abe@mypc.local>
2015-04-19 02:00:23 +0900

    update

--- lclient-core/src/test/java/org/apache/lucene/lclient/MeasureTest.java
+++ lclient-core/src/test/java/org/apache/lucene/lclient/MeasureTest.java
@@ -50,30 +50,35 @@ public class MeasureTest {
   @Test
   public void test001() throws IOException {
     System.out.println("---------- measure indexing ----------");
-        Stopwatch stopwatch = Stopwatch.createStarted();
+    Stopwatch stopwatch = Stopwatch.createStarted();
     LConnection conn = new LConnection(dataPath + sep + "db");
-        System.out.println("conn:"+stopwatch);
-        stopwatch = Stopwatch.createStarted();
+    System.out.println("conn:"+stopwatch);
+
+    stopwatch = Stopwatch.createStarted();
     LCommand cmd = new LCommand(conn, "coll", schema);
-        System.out.println("cmd:"+stopwatch);
+    System.out.println("cmd:"+stopwatch);
 
-        stopwatch = Stopwatch.createStarted();
+    stopwatch = Stopwatch.createStarted();
     IntStream.range(0, DOC_SIZE).parallel().forEach(TestUtils.getIndexConsumer(cmd));
-        System.out.println("indexing:"+stopwatch);
-        stopwatch = Stopwatch.createStarted();
-    cmd.refresh();
-        System.out.println("refreshing:"+stopwatch);
-        stopwatch = Stopwatch.createStarted();
-    cmd.commit();
-        System.out.println("committing:"+stopwatch);
+    System.out.println("indexing:"+stopwatch);
 
-        stopwatch = Stopwatch.createStarted();
+    stopwatch = Stopwatch.createStarted();
+    cmd.refresh();
+    System.out.println("refreshing:"+stopwatch);
+
+    stopwatch = Stopwatch.createStarted();
+    cmd.commit();
+    System.out.println("committing:"+stopwatch);
+
+    stopwatch = Stopwatch.createStarted();
     int count = cmd.count("*:*");
-        System.out.println("count():"+stopwatch);
+    System.out.println("count():"+stopwatch);
+
     assertThat(count, is(DOC_SIZE));
-        stopwatch = Stopwatch.createStarted();
+
+    stopwatch = Stopwatch.createStarted();
     conn.close();
-        System.out.println("closing:"+stopwatch);
+    System.out.println("closing:"+stopwatch);
   }
 
   @Test
@@ -82,50 +87,56 @@ public class MeasureTest {
     LConnection conn = new LConnection(dataPath + sep + "db");
     LCommand cmd = new LCommand(conn, "coll", schema);
 
-        Stopwatch stopwatch = Stopwatch.createStarted();
+    Stopwatch stopwatch = Stopwatch.createStarted();
     List<Document> docsA = cmd.find("*:*");
-        System.out.println("find():"+stopwatch);
+    System.out.println("find():"+stopwatch);
+
     assertThat(docsA.size(), is(DOC_SIZE));
 
-        stopwatch = Stopwatch.createStarted();
+    stopwatch = Stopwatch.createStarted();
     List<Document> docsAA = cmd.find("*:*");
-        System.out.println("find() two:"+stopwatch);
+    System.out.println("find() two:"+stopwatch);
+
     assertThat(docsAA.size(), is(DOC_SIZE));
 
-        stopwatch = Stopwatch.createStarted();
+    stopwatch = Stopwatch.createStarted();
     Document docs1 = cmd.findOne("*:*");
-        System.out.println("findOne():"+stopwatch);
+    System.out.println("findOne():"+stopwatch);
+
     assertThat(docs1, is(notNullValue()));
+
     for (IndexableField field : docs1.getFields()) {
       System.out.println("name:"+field.name()+" "+"value:"+field.stringValue());
     }
 
-        stopwatch = Stopwatch.createStarted();
+    stopwatch = Stopwatch.createStarted();
     List<Document> docsB = cmd.filter("*:*");
-        System.out.println("filter():"+stopwatch);
+    System.out.println("filter():"+stopwatch);
+
     assertThat(docsB.size(), is(DOC_SIZE));
 
-        stopwatch = Stopwatch.createStarted();
+    stopwatch = Stopwatch.createStarted();
     List<Document> docsBB = cmd.filter("*:*");
-        System.out.println("filter() two:"+stopwatch);
+    System.out.println("filter() two:"+stopwatch);
+
     assertThat(docsBB.size(), is(DOC_SIZE));
 
-        stopwatch = Stopwatch.createStarted();
+    stopwatch = Stopwatch.createStarted();
     List<Document> docsC =
       new LQuery(cmd).find("*:*").filter("*:*").limit(null).toList();
-        System.out.println("LQuery():"+stopwatch);
+    System.out.println("LQuery():"+stopwatch);
 
-        stopwatch = Stopwatch.createStarted();
+    stopwatch = Stopwatch.createStarted();
     assertThat(docsC.size(), is(DOC_SIZE));
-        System.out.println("List.size():"+stopwatch);
+    System.out.println("List.size():"+stopwatch);
 
-        stopwatch = Stopwatch.createStarted();
+    stopwatch = Stopwatch.createStarted();
     cmd.forceMerge();
-        System.out.println("forceMerging:"+stopwatch);
+    System.out.println("forceMerging:"+stopwatch);
 
-        stopwatch = Stopwatch.createStarted();
+    stopwatch = Stopwatch.createStarted();
     conn.close();
-        System.out.println("closing:"+stopwatch);
+    System.out.println("closing:"+stopwatch);
   }
 
   @Test
@@ -199,12 +210,12 @@ public class MeasureTest {
   @Test
   public void test004() throws IOException {
     System.out.println("---------- LQuery Stream test ----------");
-        Stopwatch stopwatch = Stopwatch.createStarted();
+    Stopwatch stopwatch = Stopwatch.createStarted();
 
     LConnection conn = new LConnection(dataPath + sep + "db");
     LCommand cmd = new LCommand(conn, "coll", schema);
 
-        Stopwatch stopwatch1 = Stopwatch.createStarted();
+    Stopwatch stopwatch1 = Stopwatch.createStarted();
     List<Document> first = new LQuery(cmd)
       .find("*:*")
       .filter("count:[1 TO 800]")
@@ -213,9 +224,9 @@ public class MeasureTest {
     for (Document e : first) {
       firstSum = firstSum + e.getField("price").numericValue().doubleValue();
     }
-        System.out.println("firstSum1:"+firstSum+" time:"+stopwatch1);
+    System.out.println(" firstSum1:"+firstSum+" time:"+stopwatch1);
 
-        Stopwatch stopwatch2 = Stopwatch.createStarted();
+    stopwatch1 = Stopwatch.createStarted();
     FluentIterable<Double> doubles = new LQuery(cmd)
       .find("*:*")
       .filter("count:[1 TO 800]")
@@ -229,9 +240,9 @@ public class MeasureTest {
     for (Double d : doubles) {
       dSum = dSum + d;
     }
-        System.out.println("firstSumF:"+dSum+" time:"+stopwatch2);
+    System.out.println(" firstSumF:"+dSum+" time:"+stopwatch1);
 
-        Stopwatch stopwatch3 = Stopwatch.createStarted();
+    stopwatch1 = Stopwatch.createStarted();
     double secondSum1 = StreamSupport.stream(
       /*--spliterator--*/
       Spliterators.spliterator(
@@ -244,9 +255,9 @@ public class MeasureTest {
       /*--parallel--*/ false)
       .mapToDouble(doc -> doc.getField("price").numericValue().doubleValue())
       .reduce(0, Double::sum);
-        System.out.println("secondSum1:"+secondSum1+" time:"+stopwatch3);
+    System.out.println("secondSum1:"+secondSum1+" time:"+stopwatch1);
 
-        Stopwatch stopwatch4 = Stopwatch.createStarted();
+    stopwatch1 = Stopwatch.createStarted();
     double secondSum2 = StreamSupport.stream(
       /*--spliterator--*/
       new LQuery(cmd) 
@@ -256,9 +267,9 @@ public class MeasureTest {
       /*--parallel--*/ false)
       .mapToDouble(doc -> doc.getField("price").numericValue().doubleValue())
       .reduce(0, Double::sum);
-        System.out.println("secondSum2:"+secondSum2+" time:"+stopwatch4);
+    System.out.println("secondSum2:"+secondSum2+" time:"+stopwatch1);
 
-        Stopwatch stopwatch5 = Stopwatch.createStarted();
+    stopwatch1 = Stopwatch.createStarted();
     double secondSum3 =
       new LQuery(cmd) 
         .find("*:*")
@@ -269,9 +280,9 @@ public class MeasureTest {
         //.parallel()
         .mapToDouble(doc -> doc.getField("price").numericValue().doubleValue())
         .reduce(0, Double::sum);
-        System.out.println("secondSum3:"+secondSum3+" time:"+stopwatch5);
+    System.out.println("secondSum3:"+secondSum3+" time:"+stopwatch1);
  
-        Stopwatch stopwatch6 = Stopwatch.createStarted();
+    stopwatch1 = Stopwatch.createStarted();
     double secondSum4 =
       new LQuery(cmd) 
         .find("*:*")
@@ -281,7 +292,7 @@ public class MeasureTest {
         //.parallel()
         .mapToDouble(doc -> doc.getField("price").numericValue().doubleValue())
         .reduce(0, Double::sum);
-        System.out.println("secondSum4:"+secondSum4+" time:"+stopwatch6);
+    System.out.println("secondSum4:"+secondSum4+" time:"+stopwatch1);
 
     conn.close();
 
@@ -291,18 +302,18 @@ public class MeasureTest {
   @Test
   public void test005() throws IOException {
     System.out.println("---------- Grouping test ----------");
-        Stopwatch stopwatch = Stopwatch.createStarted();
+    Stopwatch stopwatch = Stopwatch.createStarted();
 
     LConnection conn = new LConnection(dataPath + sep + "db");
     LCommand cmd = new LCommand(conn, "coll", schema);
 
-       Stopwatch stopwatch1 = Stopwatch.createStarted();
+    Stopwatch stopwatch1 = Stopwatch.createStarted();
     cmd.groupingStream("id", "id asc", "*:*", "*:*");
-       System.out.println("group by id:"+stopwatch1);
+    System.out.println("group by id:"+stopwatch1);
 
-       Stopwatch stopwatch2 = Stopwatch.createStarted();
+    stopwatch1 = Stopwatch.createStarted();
     cmd.groupingStream("tag", "tag asc", "*:*", "*:*");
-       System.out.println("group by tag:"+stopwatch2);
+    System.out.println("group by tag:"+stopwatch1);
 
     conn.close();
 
@@ -316,16 +327,16 @@ public class MeasureTest {
     LConnection conn = new LConnection(dataPath + sep + "db");
     LCommand cmd = new LCommand(conn, "coll", schema);
 
-       Stopwatch stopwatch1 = Stopwatch.createStarted();
+    Stopwatch stopwatch = Stopwatch.createStarted();
     List<ImmutablePair<ScoreDoc,Document>> docs =
       cmd.documentPairStream("*:*", "count:[1 TO 9000]", DOC_SIZE, "id asc", "id")
          .collect(Collectors.toCollection(() -> new ArrayList<>()));
     int hits = docs.size();
-       System.out.println("elapsed:"+stopwatch1);
-       System.out.println("hits:"+hits);
+    System.out.println("elapsed:"+stopwatch);
+    System.out.println("hits:"+hits);
 
     System.out.println("---------- Scan test(deep paging) ----------");
-       Stopwatch stopwatch2 = Stopwatch.createStarted();
+    stopwatch = Stopwatch.createStarted();
     int resultSize = 0;
     ScoreDoc bottom = null;
     while (true) {
@@ -337,9 +348,10 @@ public class MeasureTest {
       if (listSize == 0 || list.get(0).right == null) break;
       bottom = list.get(0).right;
     }
-       System.out.println("elapsed:"+stopwatch2);
+    System.out.println("elapsed:"+stopwatch);
     assertThat(resultSize, is(hits));
 
     conn.close();
   }
+
 }
